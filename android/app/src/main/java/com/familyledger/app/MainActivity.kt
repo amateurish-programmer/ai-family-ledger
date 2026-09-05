9c617c4b7f17f8bdfa43320860b0280a293770bf
amateurish-programmer <843067782@qq.com>
2026-09-05 23:06:48 +0800

    feat(ledger): add MainActivity.kt
    | Area | Change |
    |---|---|
    | android/app/src/main/java/com/familyledger/app/MainActivity.kt | Add initial offline ledger implementation and delivery support |
    
    Author: Wuhao

---
+++ android/app/src/main/java/com/familyledger/app/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.familyledger.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.familyledger.app.ui.*
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val model: LedgerViewModel = viewModel(factory = object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <T : ViewModel> create(modelClass: Class<T>): T =
+                    LedgerViewModel((application as LedgerApplication).repository) as T
+            })
+            FamilyLedgerTheme { LedgerApp(model) }
+        }
+    }
+}
